3a8c1dffe76fdbf4b8b130d291bef3d62e26c2dd
彭现陈 <xianchen.peng@chivox.com>
2019-03-26 13:51:45 +0800

    调整TCP KeepAlive时间

--- SocketRocket/Internal/Proxy/SRProxyConnect.m
+++ SocketRocket/Internal/Proxy/SRProxyConnect.m
@@ -352,8 +352,8 @@
                 
                 int keepAlive = 1;
                 int idleTime = 5;
-                int intvlTime = 10;
-                int cntCount = 1;
+                int intvlTime = 2;
+                int cntCount = 3;
                 int nodelay = 1;
                 if (setsockopt(socket, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(keepAlive)) < 0) {
                     NSLog(@"setsockopt SO_KEEPALIVE failed: %s", strerror(errno));
